6e8596ef9ecf64c04dc2d8708f650f2964e583ba
Akshay Bagujar <akshay.badgujar@techbulls.com>
2021-05-13 15:02:30 +0530

    Plus Minus

---
+++ src/main/java/com/hackerrank/problem_solving/Plus_Minus/Result.java
@@ -0,0 +1,28 @@
+package com.hackerrank.problem_solving.Plus_Minus;
+
+import java.util.List;
+
+public class Result {
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+        double positiveCount = 0, negativeCount =0, zeroCount =0;
+        for(Integer integer:arr){
+            if(integer>0){
+                positiveCount++;
+            }else if(integer<0){
+                negativeCount++;
+            }else {
+                zeroCount++;
+            }
+        }
+        System.out.printf("%.6f%n",(positiveCount/arr.size()));
+        System.out.printf("%.6f%n",(negativeCount/arr.size()));
+        System.out.printf("%.6f%n",(zeroCount/arr.size()));
+    }
+}
