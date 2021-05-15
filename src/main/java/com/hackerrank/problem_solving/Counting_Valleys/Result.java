ea2128a128d34ecd5492d7807d5e43222496ea46
Akshay Bagujar <akshay.badgujar@techbulls.com>
2021-05-15 20:06:34 +0530

    Counting Valleys

---
+++ src/main/java/com/hackerrank/problem_solving/Counting_Valleys/Result.java
@@ -0,0 +1,36 @@
+package com.hackerrank.problem_solving.Counting_Valleys;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Result {
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+        // Write your code here
+        int result = 0;
+        int level = 0;
+        int valley = 0;
+        String[] pathArray = path.split("");
+        for(int index =0;index<pathArray.length;index++){
+            if(pathArray[index].equalsIgnoreCase("D")){
+                level--;
+            }else {
+                level++;
+                if(level == 0){
+                    valley++;
+                }
+            }
+        }
+        result = valley;
+        return result;
+    }
+}
