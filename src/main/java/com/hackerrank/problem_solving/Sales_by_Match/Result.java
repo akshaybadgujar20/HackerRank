2015982cb6da15e0a3f4956ac993cbfbbac94cdf
Akshay Bagujar <akshay.badgujar@techbulls.com>
2021-05-15 19:51:04 +0530

    Sales by Match

---
+++ src/main/java/com/hackerrank/problem_solving/Sales_by_Match/Result.java
@@ -0,0 +1,35 @@
+package com.hackerrank.problem_solving.Sales_by_Match;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Result {
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        // Write your code here
+        int result =0;
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        for(Integer integer:ar){
+            if(hashMap.get(integer) == null){
+                hashMap.put(integer,1);
+            }else {
+                int temp = hashMap.get(integer);
+                hashMap.put(integer,++temp);
+            }
+        }
+        for(Map.Entry<Integer,Integer> map:hashMap.entrySet()){
+            result += map.getValue()/2;
+        }
+        return result;
+    }
+}
