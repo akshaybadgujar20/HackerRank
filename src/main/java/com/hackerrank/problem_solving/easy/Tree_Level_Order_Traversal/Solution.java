831e319a6447ae012c2e220f17371c74ff74db5a
Akshay Bagujar <akshay.badgujar@techbulls.com>
2021-06-24 20:05:00 +0530

    Tree: Level Order Traversal

---
+++ src/main/java/com/hackerrank/problem_solving/easy/Tree_Level_Order_Traversal/Solution.java
@@ -0,0 +1,69 @@
+package com.hackerrank.problem_solving.easy.Tree_Level_Order_Traversal;
+
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+    /*
+
+    class Node
+        int data;
+        Node left;
+        Node right;
+    */
+    static Queue<Node> queue = new ArrayDeque<>();
+    public static void levelOrder(Node root) {
+        System.out.print(root.data+" ");
+        if(root.left!= null){
+            queue.add(root.left);
+        }
+        if(root.right!= null){
+            queue.add(root.right);
+        }
+        Node temp = queue.poll();
+        if(temp!=null){
+            levelOrder(temp);
+        }
+    }
+
+    public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        levelOrder(root);
+    }
+}
